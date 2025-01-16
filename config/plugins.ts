da815476b48b3d4f92e3ba006533a1418fe4179a
minhthuan1809 <thuan1809minh@gmail.com>
2025-01-16 09:54:39 +0700

    fix anh

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,15 @@
 export default () => ({});
+
+module.exports = {
+    upload: {
+        config: {
+            provider: 'local', // Change this to your desired provider, e.g., 'aws-s3'
+            providerOptions: {
+                sizeLimit: 1000000, // Giới hạn kích thước tệp (ví dụ: 1MB)
+                // Add other provider-specific options here
+                // For local provider, you can specify the storage path
+                path: '/path/to/store/images', // Change this to your desired storage path
+            },
+        },
+    },
+};
