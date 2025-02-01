8d7a366a89bbd7e9c090148818e876d0c01c2f11
minhthuan1809 <thuan1809minh@gmail.com>
2025-02-01 21:50:06 +0700

    edit code

--- src/api/exam/controllers/exam.ts
+++ src/api/exam/controllers/exam.ts
@@ -4,4 +4,69 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::exam.exam');
+export default factories.createCoreController('api::exam.exam', ({strapi}) => ({
+  // Create a new exam
+  async create(ctx) {
+    try {
+      const { data } = ctx.request.body;
+      const result = await strapi.entityService.create('api::exam.exam', {
+        data: data
+      });
+      return { data: result };
+    } catch (err) {
+      ctx.throw(500, err);
+    }
+  },
+
+  // Find all exams
+  async find(ctx) {
+    try {
+      const exams = await strapi.entityService.findMany('api::exam.exam', {
+        ...ctx.query,
+      });
+      return { data: exams };
+    } catch (err) {
+      ctx.throw(500, err);
+    }
+  },
+
+  // Find one exam by ID
+  async findOne(ctx) {
+    try {
+      const { id } = ctx.params;
+      const exam = await strapi.entityService.findOne('api::exam.exam', id, {
+        ...ctx.query,
+      });
+      return { data: exam };
+    } catch (err) {
+      ctx.throw(500, err);
+    }
+  },
+
+  // Update an exam
+  async update(ctx) {
+    try {
+      const { id } = ctx.params;
+      const { data } = ctx.request.body;
+      
+      const result = await strapi.entityService.update('api::exam.exam', id, {
+        data: data
+      });
+      return { data: result };
+    } catch (err) {
+      ctx.throw(500, err);
+    }
+  },
+
+  // Delete an exam
+  async delete(ctx) {
+    try {
+      const { id } = ctx.params;
+      const result = await strapi.entityService.delete('api::exam.exam', id);
+      return { data: result };
+    } catch (err) {
+      ctx.throw(500, err);
+    }
+  },
+}));
+
--- src/api/exam/routes/exam.ts
+++ src/api/exam/routes/exam.ts
@@ -4,4 +4,32 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::exam.exam');
+export default factories.createCoreRouter('api::exam.exam', {
+  config: {
+    find: {
+      auth: false,
+      policies: [],
+      middlewares: [],
+    },
+    findOne: {
+      auth: false,
+      policies: [],
+      middlewares: [], 
+    },
+    create: {
+      auth: false,
+      policies: [],
+      middlewares: [],
+    },
+    update: {
+      auth: false,
+      policies: [],
+      middlewares: [],
+    },
+    delete: {
+      auth: false,
+      policies: [],
+      middlewares: [],
+    },
+  }
+});
